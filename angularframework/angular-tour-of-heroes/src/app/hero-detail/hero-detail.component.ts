f8054005bb362b1e5792f25af1dcf449c09d0617
Brahian <brahian.verac@gmail.com>
2022-07-27 21:47:04 -0500

    working with routes

--- angularframework/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angularframework/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,3 +1,7 @@
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService} from '../hero.service';
+
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 
@@ -10,9 +14,19 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id  = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero=>this.hero = hero);
   }
 
 }
