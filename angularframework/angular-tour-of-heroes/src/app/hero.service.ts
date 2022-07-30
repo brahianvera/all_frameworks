ad7c1282fb4d3df03513dfd2f1d4d22cb6f27a1c
Brahian <brahian.verac@gmail.com>
2022-07-29 22:23:41 -0500

    getting heroes with  http

--- angularframework/angular-tour-of-heroes/src/app/hero.service.ts
+++ angularframework/angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,28 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
-
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]>{
-    const heroes = of(HEROES)
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero>{
-
     const hero = HEROES.find(h => h.id === id);
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`HeroService: fetched hero id=${id}`);
     return of(hero!);
-
   }
+
+  
+  private log(message: string){
+    this.messageService.add(`HeroService: ${message}`);
+  };
+
+  
+
 }
