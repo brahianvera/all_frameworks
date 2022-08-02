d812a65844afa9dc201288d3b9012289ad12caa7
Brahian <brahian.verac@gmail.com>
2022-08-01 22:12:57 -0500

    adding tap herror and handle error

--- angularframework/angular-tour-of-heroes/src/app/hero.service.ts
+++ angularframework/angular-tour-of-heroes/src/app/hero.service.ts
@@ -19,9 +19,12 @@ export class HeroService {
   constructor(private http: HttpClient,
     private messageService: MessageService) { }
 
+  
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]>{
     return this.http.get<Hero[]>(this.heroesUrl)
     .pipe(
+      tap(_ => this.log('fetched heroes')),
       catchError(this.handleError<Hero[]>('getHeroes',[]))
     );
   }
@@ -36,6 +39,28 @@ export class HeroService {
   private log(message: string){
     this.messageService.add(`HeroService: ${message}`);
   };
+
+  /**
+   * Handle Http operation that failed
+   * Let the app continue 
+   * 
+   * @param operation  - name of the operation that failed
+   * @param result - optional value to return as the observable result.
+   * @returns 
+   */
+  private handleError<T>(operation = 'operation', result?:T){
+    return (error: any): Observable<T>=>{
+      //TODO: send the error  to remote logging infraestructura 
+      console.error(error); // log to console instead
+
+      //TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      //let the app keep running by returning an empty result.
+      return of(result as T);
+    }
+
+  }
 
   
 
