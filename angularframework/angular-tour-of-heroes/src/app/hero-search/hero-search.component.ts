a3ed76dac093fa47568b218781cb63655346ed54
Brahian <brahian.verac@gmail.com>
2022-08-09 22:13:36 -0500

    finished de hero tours

--- angularframework/angular-tour-of-heroes/src/app/hero-search/hero-search.component.ts
+++ angularframework/angular-tour-of-heroes/src/app/hero-search/hero-search.component.ts
@@ -15,9 +15,33 @@ import {HeroService} from '../hero.service';
 })
 export class HeroSearchComponent implements OnInit {
 
-  constructor() { }
+  heroes$!: Observable<Hero[]>
+  //Subject can act as both observable & an observer.
+  //please see this page 
+  // https://www.tektutorialshub.com/angular/subjects-in-angular/
+  private searchTerms = new Subject<string>();
+
+
+  constructor(private heroService: HeroService) { }
+
+  search(term: string): void{
+    this.searchTerms.next(term);
+  }
 
   ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      //wait  300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      //ignore new term if same as previous form
+      distinctUntilChanged(),
+
+      //switch to new search observable each time the term changes
+      //whit switchMap i can make a many request but it all return the result
+      // of the most recent http method call. Result from prior calls  are canceled and discared
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+
+    )
   }
 
 }
