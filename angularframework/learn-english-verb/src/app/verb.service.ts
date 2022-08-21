6c6eda3fdf24573794073f02d5bf58d4feccb03b
Brahian <brahian.verac@gmail.com>
2022-08-20 23:34:25 -0500

    adding verb service

---
+++ angularframework/learn-english-verb/src/app/verb.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Verb } from './verb';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VerbService {
+
+  constructor() { }
+}
