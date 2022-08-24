b7e6760a668b02df91a2241798b8d718d985ad64
Brahian <brahian.verac@gmail.com>
2022-08-23 22:45:53 -0500

    service to consult a hero by ID

--- angularframework/learn-english-verb/src/app/verb.service.ts
+++ angularframework/learn-english-verb/src/app/verb.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Verb } from './verb';
-
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VerbService {
+  private verbsUrl = "api/verb";
+  
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type':'application/json'})
+  }
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+
+  getVerb(id: number): Observable<Verb>{
+    const url =`${this.verbsUrl}/${id}`;
+    return this.http.get<Verb>(url);
+  }
 }
