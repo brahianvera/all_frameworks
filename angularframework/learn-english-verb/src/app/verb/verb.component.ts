0c9834a5560f471a8ddc0e1c74621525b7893108
Brahian <brahian.verac@gmail.com>
2022-08-24 22:39:28 -0500

    subscribed getId to service function

--- angularframework/learn-english-verb/src/app/verb/verb.component.ts
+++ angularframework/learn-english-verb/src/app/verb/verb.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit,Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Verb } from '../verb';
+import { VerbService } from '../verb.service';
 
 @Component({
   selector: 'app-verb',
@@ -7,13 +9,24 @@ import { Verb } from '../verb';
   styleUrls: ['./verb.component.css']
 })
 export class VerbComponent implements OnInit {
-  verb:Verb;
+  @Input() verb?:Verb;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private verbService: VerbService
+  ) { }
 
 
   ngOnInit(): void {
+    this.getHero();
+  }
 
+  getHero(): void{
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    // the verb brefore at the equal an lower sing means that is a parameter of a arrow function
+    // the this.verb is a verb of this class 
+    // the las verb is the parameter previously give to function arrow
+    this.verbService.getVerb(id).subscribe(verb => this.verb = verb);
   }
 
 }
