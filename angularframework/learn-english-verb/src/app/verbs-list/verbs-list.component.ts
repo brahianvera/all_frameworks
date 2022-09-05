ba1307c3e7703ff0d34fcc34b0f631362e62076b
Brahian <brahian.verac@gmail.com>
2022-09-05 09:11:43 -0500

    component to list all verbs

--- angularframework/learn-english-verb/src/app/verbs-list/verbs-list.component.ts
+++ angularframework/learn-english-verb/src/app/verbs-list/verbs-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { VerbService } from '../verb.service';
+import { Verb } from '../verb';
 
 @Component({
   selector: 'app-verbs-list',
   templateUrl: './verbs-list.component.html',
   styleUrls: ['./verbs-list.component.css']
 })
+
 export class VerbsListComponent implements OnInit {
 
-  constructor() { }
+  verbs: Verb[] = [];
+  //inject code in the constructor, dependency injection
+  constructor(
+    private verbService: VerbService
+  ) { }
 
   ngOnInit(): void {
+
+  }
+
+  getVerbs(): void{
+    this.verbService.getVerbs().subscribe(Verbs => this.verbs = Verbs);
   }
 
 }
