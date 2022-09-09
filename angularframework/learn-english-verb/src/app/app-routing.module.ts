69a4bac46d97517c01b4613c387c3779eb23a4f9
Brahian <brahian.verac@gmail.com>
2022-09-08 22:15:33 -0500

    addimg new verb form

--- angularframework/learn-english-verb/src/app/app-routing.module.ts
+++ angularframework/learn-english-verb/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import {  RouterModule, Routes } from '@angular/router';
 import { VerbComponent } from './verb/verb.component';
 import { VerbsListComponent } from './verbs-list/verbs-list.component';
+import { NewVerbComponent } from './new-verb/new-verb.component';
 
 const routes: Routes = [
   { path: 'verb/:id', component:VerbComponent},
-  { path: 'verbs', component: VerbsListComponent}
+  { path: 'verbs', component: VerbsListComponent},
+  { path: 'newverb', component: NewVerbComponent }
 ];
 
 
