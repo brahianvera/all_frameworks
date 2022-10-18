893844c743685c9b6e3b96ce9eee5a167d710cef
Brahian <brahian.verac@gmail.com>
2022-10-17 22:42:21 -0500

    adding styles css to webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,10 +1,13 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import './style.css';
+
 
 function component() {
     const element = document.createElement('div');
   
     // lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
   
     return element;
   }
