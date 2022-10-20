0595b05d90c2e8b9dc3df0ef3f0aa939fcc49e31
Brahian <brahian.verac@gmail.com>
2022-10-19 23:15:46 -0500

    adding fonts whit webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|ttf|woff2|eot|otf)$/,
+                type: 'asset/resource'
             }
         ]
     }
